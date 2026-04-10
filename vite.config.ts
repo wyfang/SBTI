da58907fa77b77c9a5b764b38f6d4001a7d353f0
Wi-Fi <hello@csgo.email>
2026-04-10 13:22:08 +0800

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    plugins: [react(), tailwindcss()],
     base: './',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
